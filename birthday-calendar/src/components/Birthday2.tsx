114e092d8bae536f71c690cea9e2a3062a5fab1b
iamdebobrota <iamdebobrota@gmail.com>
2024-03-28 21:13:39 +0530

    update: types are updated

--- birthday-calendar/src/components/Birthday2.tsx
+++ birthday-calendar/src/components/Birthday2.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import data from "../../db.json";
 
-const Birthday2 = () => {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+interface ItemType {
+  name: string;
+  birthday: string;
+}
+
+const Birthday2: React.FC = () => {
+  const days: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return (
     <div>
@@ -16,15 +21,17 @@ const Birthday2 = () => {
   );
 };
 
-const DayCard = ({ items, day }) => {
-  let newItems = items
+const DayCard = ({ items, day }: { items: ItemType[]; day: string }) => {
+  const newItems: ItemType[] = items
     .filter((el) => {
-      let date = new Date(el.birthday).toDateString().split(" ")[0];
-      return date.toLocaleLowerCase() == day.toLocaleLowerCase();
+      const date = new Date(el.birthday).toDateString().split(" ")[0];
+      return date.toLocaleLowerCase() === day.toLocaleLowerCase();
     })
-    .sort(
-      (a, b) => new Date(a.birthday).valueOf() < new Date(b.birthday).valueOf()
-    );
+    .sort((a, b) => {
+      const dateA = new Date(a.birthday).valueOf();
+      const dateB = new Date(b.birthday).valueOf();
+      return dateA - dateB;
+    });
 
   return (
     <div className="card">
@@ -41,8 +48,8 @@ const DayCard = ({ items, day }) => {
             newItems.length >= 2 ? `repeat(2, 1fr)` : `repeat(1, 1fr)`,
         }}>
         {newItems.length > 0 ? (
-          newItems.map((el, i) => {
-            return <PersonCard key={el + i} indx={i} person={el.name} />;
+          newItems.map((el, i) => { 
+            return <PersonCard key={el.name} indx={i} person={el.name} />;
           })
         ) : (
           <img
@@ -50,7 +57,6 @@ const DayCard = ({ items, day }) => {
               height: "100px",
               width: "180px",
               objectFit: "cover",
-              mixBlendMode: "",
             }}
             src="https://w7.pngwing.com/pngs/432/660/png-transparent-empty-cart-illustration-thumbnail.png"
             alt="img"
@@ -61,16 +67,16 @@ const DayCard = ({ items, day }) => {
   );
 };
 
-const PersonCard = ({ indx, person }) => {
-  person = person.split(" ");
-  let colors = ["crimson", "yellow", "Green", "orange"];
+const PersonCard = ({ indx, person }: { indx: number; person: string }) => {
+  const newperson: string[] = person.split(" ");
+  const colors: string[] = ["crimson", "yellow", "Green", "orange"];
 
   return (
     <div
       className="person"
       style={{ background: colors[indx % colors.length] }}>
-      {person[0][0]}
-      {person[1][0]}{" "}
+      {newperson[0][0]}
+      {newperson[1][0]}{" "}
     </div>
   );
 };
